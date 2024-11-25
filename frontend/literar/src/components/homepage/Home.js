5f21925ba578cdecf883d4eb42a952d1b2f4b899
GabrielWesz <54816775+GabrielWesz@users.noreply.github.com>
2024-11-24 23:33:16 -0300

    Algumas mudanças na organização da pasta do front

---
+++ frontend/literar/src/components/homepage/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+    return (
+
+        <div className='name'>
+            <h1>Hello Name</h1>
+        </div>
+    )
+}
+
+export default Home
