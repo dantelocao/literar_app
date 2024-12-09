3b719b070bd82f04e6b85e3395b6b753c6462725
Dante Dardaque <dante.dardaque@hotmail.com>
2024-12-09 12:08:23 -0300

    trocando localhost por ip do servidor

--- client/src/utils/api/api.js
+++ client/src/utils/api/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1",
+    baseURL: "http://34.172.77.82:5000/api/v1",
 })
 
 export const getTimelinePosts = (username) => API.get(`/posts/get-timeline-posts/${username}`)
 export const getAllPosts = () => API.get("/posts")
-
 export const getAllBooks = () => API.get('/books/get-books')
 export const getUserData = (userId) => API.get(`/users/${userId}`)
 export const getUserProfileData = (username) => API.get(`/users?username=${username}`)
